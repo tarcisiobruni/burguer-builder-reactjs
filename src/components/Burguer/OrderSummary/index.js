7daf41e771a524602c9ca961f531528ecd22e8e3
tarcisiobruni <tarcisiobruni@hotmail.com>
2020-05-17 09:44:07 -0300

    perf(order): add shouldcomponentupdate to modal

--- src/components/Burguer/OrderSummary/index.js
+++ src/components/Burguer/OrderSummary/index.js
@@ -1,32 +1,37 @@
-import React from "react";
-
+import React, { Component } from "react";
 import Auxiliary from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button";
 
-export default function OrderSummary(props) {
-    const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+export default class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log("[Order Sumary will Update]");
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+                    {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
         return (
-            <li key={igKey}>
-                <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-                {props.ingredients[igKey]}
-            </li>
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burguer with the following ingredients:</p>
+                <ul>{ingredientSummary}</ul>
+                <p>
+                    <strong>Total Price : {this.props.price.toFixed(2)}</strong>
+                </p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+                    CANCEL
+                </Button>
+                <Button btnType="Success" clicked={this.props.purchasedContinued}>
+                    CONTINUE
+                </Button>
+            </Auxiliary>
         );
-    });
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burguer with the following ingredients:</p>
-            <ul>{ingredientSummary}</ul>
-            <p>
-                <strong>Total Price : {props.price.toFixed(2)}</strong>
-            </p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>
-                CANCEL
-            </Button>
-            <Button btnType="Success" clicked={props.purchasedContinued}>
-                CONTINUE
-            </Button>
-        </Auxiliary>
-    );
+    }
 }
--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import styles from './modal.module.css';
-import Auxiliary from '../../../hoc/Auxiliary';
-import Backdrop from '../Backdrop';
+import React, { Component } from "react";
+import styles from "./modal.module.css";
+import Auxiliary from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop";
 
-export default function Modal(props) {
-  return (
-    <Auxiliary>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0',
-        }}
-        className={styles.Modal}
-      >
-        {props.children}
-      </div>
-    </Auxiliary>
-  );
+export default class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log("[Modal] WillUpdate");
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    style={{
+                        transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                        opacity: this.props.show ? "1" : "0",
+                    }}
+                    className={styles.Modal}
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
 }
