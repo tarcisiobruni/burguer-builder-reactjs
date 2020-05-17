bccad1e9a5075526690999ad7038c1207a2acbd6
tarcisiobruni <tarcisiobruni@hotmail.com>
2020-05-17 09:16:53 -0300

    refact(pro-types): add prop types to build control

--- src/components/Burguer/BuildControls/BuildControl/index.js
+++ src/components/Burguer/BuildControls/BuildControl/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import styles from './build-control.module.css'
-export default function BuildControl(props){
-    const {added,removed,disable} = props;
+import React from "react";
+import styles from "./build-control.module.css";
+import PropTypes from "prop-types";
+
+export default function BuildControl(props) {
+    const { added, removed, disable } = props;
     return (
         <div className={styles.BuildControl}>
-            <div className={styles.Label}>
-                {props.label}
-            </div>
-            <button disabled={disable} className={styles.Less} onClick={removed}>Less</button>
-            <button className={styles.More} onClick={added}>More</button>
+            <div className={styles.Label}>{props.label}</div>
+            <button disabled={disable} className={styles.Less} onClick={removed}>
+                Less
+            </button>
+            <button className={styles.More} onClick={added}>
+                More
+            </button>
         </div>
-    )
+    );
 }
+
+BuildControl.propTypes = {
+    added: PropTypes.func.isRequired,
+    removed: PropTypes.func.isRequired,
+    disable: PropTypes.bool,
+    label: PropTypes.string.isRequired,
+};
